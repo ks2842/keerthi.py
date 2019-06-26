017b4d79fa4a00aba6817735643cadd7d51b2000
ks2842 <51949666+ks2842@users.noreply.github.com>
2019-06-26 15:43:02 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+ch=input()
+if('a','A','e','E','i','i','o','O','u','U'):
+   print("vowel")
+elif('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):  
+   print("consonsnt")
+else:
+   print("Invalid")
